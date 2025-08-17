91200ec0e71cd2cde9e8045d8717fb28bad1321d
Krystian Krasowski <krystiankrasowski@gmail.com>
2025-08-17 23:13:56 +0200

    Turning off cursorline on window switching

--- neovim/.config/nvim/lua/user/options.lua
+++ neovim/.config/nvim/lua/user/options.lua
@@ -10,3 +10,15 @@ vim.o.updatetime = 300
 
 vim.opt.path:append("**")
 vim.opt.wildignore:append("**/build/**")
+
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+  callback = function()
+    vim.wo.cursorline = true
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "WinLeave" }, {
+  callback = function()
+    vim.wo.cursorline = false
+  end,
+})
