41deb9602ce2b13d3467ac1837047bc79daafca5
Krystian Krasowski <krystiankrasowski@gmail.com>
2026-05-17 09:43:23 +0200

    minor changes in jdtls config

--- neovim/.config/nvim/lsp/jdtls.lua
+++ neovim/.config/nvim/lsp/jdtls.lua
@@ -2,22 +2,26 @@ local home = vim.env.HOME
 local jdtls_path = home .. "/.local/share/jdtls"
 local launcher_jar = vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
 
--- define markers once
 local root_markers = {
     ".git",
+    ".gitignore",
     "mvnw",
     "gradlew",
     "pom.xml",
+    "settings.gradle.kts",
+    "build.gradle.kts",
+    "settings.gradle",
     "build.gradle",
 }
 
 -- detect project root
 local root_dir = vim.fs.root(0, root_markers)
 if not root_dir then
-    root_dir = vim.fn.getcwd() -- fallback if nothing matches
+    -- fallback if nothing matches
+    root_dir = vim.fn.getcwd()
 end
 
--- put workspace inside project as ".jdtls/"
+-- put workspace in it's own separate area
 local workspace_dir = home .. "/.jdtls"
 
 return {
@@ -32,9 +36,8 @@ return {
         "--add-modules=ALL-SYSTEM",
         "--add-opens", "java.base/java.util=ALL-UNNAMED",
         "--add-opens", "java.base/java.lang=ALL-UNNAMED",
-
         "-jar", launcher_jar,
-        "-configuration", jdtls_path .. "/config_linux", -- or config_mac/config_win
+        "-configuration", jdtls_path .. "/config_linux",
         "-data", workspace_dir,
     },
 
